44eb33207e605501f1b8630fcbaa75522880fd78
SSRICODE <736550258@qq.com>
2018-05-31 16:49:09 +0800

    更改一些获取元素的规则，避免异常 编写利用多线程测试获取全部优惠页面的信息

--- monitor/Spy.go
+++ monitor/Spy.go
@@ -5,11 +5,11 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"io"
-	"steam-discount/common"
 	"strings"
 	"time"
 	"io/ioutil"
 	"math/rand"
+	"strconv"
 )
 
 
@@ -18,6 +18,9 @@ const (
 	MAC
 	LINUX
 )
+
+//id可能会存在的标签
+var idAttrNames = [...] string{"data-ds-packageid","data-ds-appid","data-ds-bundleid"}
 
 //代理
 var userAgent = [...]string{"Mozilla/5.0 (compatible, MSIE 10.0, Windows NT, DigExt)",
@@ -52,34 +55,52 @@ type MonitorContent struct {
 }
 
 //获取网页内容
-func GetContent(url string) ( contents []MonitorContent){
+func GetContent(url string , contents chan MonitorContent,pageSize chan int,maxContentSize chan int){
+	defer func() {	//用于recover可能会出现的panic
+		if r := recover(); r != nil {
+			fmt.Println("getContentPanic", r)
+		}
+	}()
+	fmt.Println("now is ",url)
 	request,_ := http.NewRequest("GET",url,nil)
 	request.Header.Set("User-Agent", getRandomUserAgent())
 	client := http.DefaultClient
 	res, err := client.Do(request)
 	if err != nil {
-		fmt.Errorf("Get请求返回错误:", url, err.Error())
+		fmt.Println("Get请求返回错误:", url, err.Error())
 		return
 	}
 	if res.StatusCode==200 {
 		body := res.Body
-		contents = finTargetedContent(body)
+		finTargetedContent(body,contents,pageSize,maxContentSize)
 		defer body.Close()
 	}
-	return contents
 }
 
 //查找目标内容并放入MonitorContent内
-func finTargetedContent(content io.Reader) ( contents []MonitorContent){
+func finTargetedContent(content io.Reader,contents chan MonitorContent,pageSize chan int,maxContentSize chan int){
 	reader ,err := goquery.NewDocumentFromReader(content)
 	if err!=nil {
 		fmt.Println(err.Error())
-		return nil
+		return
+	}
+
+
+	if pageSize!=nil {
+		pSize,contentSize := getPageSizeAndMaxContentSize(reader)
+		pageSize <- pSize
+		maxContentSize <- contentSize
 	}
 
 	reader.Find("#search_result_container div").Eq(1).Find("a").Each(func(i int , tag *goquery.Selection){
 		content := MonitorContent{}
-		id,_ := tag.Attr("data-ds-appid")
+
+		var id string
+		var exists bool
+		for i:=0 ; i<len(idAttrNames) && !exists ; i++ {
+			id,exists = tag.Attr(idAttrNames[i])
+		}
+
 		name := tag.Find(".search_name .title").Text()
 		payUrl,_ := tag.Attr("href")
 		thumbnail,_ := tag.Find(".search_capsule img").Attr("src")
@@ -98,16 +119,21 @@ func finTargetedContent(content io.Reader) ( contents []MonitorContent){
 			}
 		})
 
-		content.Id = common.StrToInt(id)
+		content.Id,_ = strconv.Atoi(id)
 		content.Name = name
 		content.PayUrl = payUrl
 		content.Thumbnail = downPicture(thumbnail,id)
 		content.IssueDate = formatDate(issueDate)
-		content.Off = strings.TrimSpace(off)	//去除分行符和空格
+		//去除分行符和空格、‘-’和‘%’符号
+		content.Off = strings.TrimSpace(off)
+		if content.Off!="" {
+			content.Off = content.Off[1:len(content.Off)-1]
+		}
+
 		content.Price,content.AtferOffPrice = separatePrices(prices)
 		content.SupportPlatforms = supportPlatforms
 
-		contents = append(contents,content)
+		contents <- content
 	})
 	return
 }
@@ -118,14 +144,17 @@ func separatePrices(prices string)(price float64 , offPrice float64){
 	pricesSlice := strings.Split(prices,"¥")
 	size := len(pricesSlice)
 	if size==3 {
-		price = common.StrToFloat64(strings.TrimSpace(pricesSlice[1]))
-		offPrice = common.StrToFloat64(strings.TrimSpace(pricesSlice[2]))
+		price,_ = strconv.ParseFloat(strings.TrimSpace(pricesSlice[1]),32)
+		offPrice,_ = strconv.ParseFloat(strings.TrimSpace(pricesSlice[2]),32)
 	}
 	return
 }
 
 //通过格式化日期字符串为time，再由time格式化为想要的格式的string
 func formatDate(dateStr string) (formatStr string){
+	if dateStr=="" {
+		return
+	}
 	v,e := time.Parse("2 Jan, 2006",dateStr)
 	if e!=nil {
 		fmt.Println(e.Error())
@@ -161,5 +190,23 @@ func downPicture(pictureUrl string,gameIdStr string)(localUrl string){
 //利用随机数，随机获取一个代理
 func getRandomUserAgent() string {
 	return userAgent[rand.Intn(len(userAgent))]
+}
+
+func getPageSizeAndMaxContentSize(reader *goquery.Document)(pageSize int ,maxContentSize int){
+	contentSizeStr := reader.Find("#search_result_container .search_pagination .search_pagination_left").Text()
+
+	pageButtonSelection := reader.Find("#search_result_container .search_pagination .search_pagination_right a")
+
+	//倒数第二个a标签是最后一页的页码
+	pageStr := pageButtonSelection.Eq(pageButtonSelection.Length()-2).Text()
+
+	pageSize,_ = strconv.Atoi(pageStr)
+
+	contentSizeStr = strings.TrimSpace(contentSizeStr)
+	//去除空格之后`showing 1 - 25 of 1434`，以`of`为分隔符拆分字符串，取到` 1434 `,最后去除空格
+	contentSizeStr = strings.TrimSpace(strings.Split(contentSizeStr,"of")[1])
+
+	maxContentSize ,_ = strconv.Atoi(contentSizeStr)
+	return
 }
 
--- monitor/Spy_test.go
+++ monitor/Spy_test.go
@@ -2,12 +2,49 @@ package monitor
 
 import (
 	"testing"
+	"strconv"
 	"fmt"
 )
 
+//
 func Test_GetContent(t *testing.T){
-	result := GetContent("https://store.steampowered.com/search/?specials=1");
-	for _,v := range result{
-		fmt.Println(v.Thumbnail)
+	var contentChannel = make(chan MonitorContent,50)
+	var pageSizeChannel = make(chan int )
+	var maxContentSize = make(chan int)
+
+	go readPageSizeChannel(pageSizeChannel,contentChannel)
+
+	go GetContent("https://store.steampowered.com/search/?specials=1",contentChannel,pageSizeChannel,maxContentSize)
+
+	readContentChannel(contentChannel,maxContentSize)
+
+}
+
+//当pageChan有值时才进行读操作（即，进行第一次爬虫获取到这个值的时候；启动多个goroutine继续爬取之后的页面数据
+func readPageSizeChannel(pageChan chan int,contentChannel chan MonitorContent){
+	maxI,ok := <- pageChan
+	if ok {
+		for i:=2; i<=maxI ;i++  {
+			go GetContent("https://store.steampowered.com/search/?specials=1&page="+strconv.Itoa(i),contentChannel,nil,nil)
+		}
+		close(pageChan)
+	}
+}
+
+//当maxContentSize有值时才进行读操作（即，进行第一次爬虫获取到这个值的时候
+func readContentChannel(contentChannel chan MonitorContent,maxContentSize chan int){
+	i := 0
+	var size int
+	if v,f :=<-maxContentSize;f{
+		size = v
+		fmt.Println("contentSize",size)
+		close(maxContentSize)
+	}
+	for v := range contentChannel{
+		i++
+		fmt.Println(v.Thumbnail,i)
+		if size == i {
+			close(contentChannel)
+		}
 	}
 }
